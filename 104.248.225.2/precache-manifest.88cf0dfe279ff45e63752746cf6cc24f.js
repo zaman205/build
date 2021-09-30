7f5df2407482e33a0f870ffa2f1fd4f31c1041f0
Your Name <zaman1989.com>
2021-09-30 05:52:45 +0000

    first commit

---
+++ 104.248.225.2/precache-manifest.88cf0dfe279ff45e63752746cf6cc24f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bec68acc504c242a3f54d099615eb85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f09f00b15728dc043ee3",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "a91a60cc7879c1c95f2b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f09f00b15728dc043ee3",
+    "url": "/static/js/2.634b7570.chunk.js"
+  },
+  {
+    "revision": "d81c6c91afd2bb4bf8caccddf0472235",
+    "url": "/static/js/2.634b7570.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a91a60cc7879c1c95f2b",
+    "url": "/static/js/main.347871de.chunk.js"
+  },
+  {
+    "revision": "f799113acf86ede46e25",
+    "url": "/static/js/runtime-main.56699e5a.js"
+  }
+]);
